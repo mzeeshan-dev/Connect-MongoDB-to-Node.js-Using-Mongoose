fe0cb8fda8dcec95dd7275b38bc3b4cda4bc759f
Muhammad Zeeshan <regalcoderz@gmail.com>
2021-10-02 18:10:18 +0500

    Schema & Model

--- src/app.js
+++ src/app.js
@@ -8,3 +8,23 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+// Schema
+
+const userlistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: String,
+  age: Number,
+  active: Boolean,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Model
+
+const Userlist = new mongoose.model("Userlist", userlistSchema); // => Collection Creation
